16e88cc4db50ecfd316ebb21c8c3fc3300ad36d5
Alex Chojnacki <thealex@umich.edu>
2016-06-09 21:47:35 -0700

    Added a script and updated about page
    The script is really messy, but it's super short so it doesn't really
    matter. It takes the spreadsheet from google docs (assumed to be named
    'sheet.csv' in the working dir--google doesn't have a drive API) and
    outputs (to STDOUT) some markdown content of all of the people in that
    spreadsheet. Makes it easier to keep the active roster up to date. All
    you gotta do is put out a google form at the beginning of the semester,
    run the script `python membership.py | pbcopy` (on mac) and suddenly the
    markdown is on your clipboard.

---
+++ scripts/membership.py
@@ -0,0 +1,27 @@
+# This script takes a spreadsheet that can be found on google drive
+# and converts the data into markdown for easy posting to the about page.
+
+from pandas import read_csv
+
+df = read_csv('sheet.csv')
+# Convert the table to a list of lists. Each list is a row from the spreadsheet.
+rows = list(map(list, df.values))
+
+for person in rows:
+    # first two cols in sheet.csv are timestamp and email
+    comps = person[2]
+    title = person[3]
+    blurb = person[4]
+    link = person[5]
+    name = person[6]
+    post = """
+### %(name)s
+
+* _%(title)s_
+* Competitions Completed: _%(comps)s_
+* _%(blurb)s_
+* [Personal Page](%(link)s)
+
+---""" % locals()
+    print(post)
+
